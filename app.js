7d85833d2ec5c2ccb500881b6059ae95397696bf
Your Name <naseribrahim755@gmail.com>
2025-05-05 10:59:31 -0500

    add local strategy

--- app.js
+++ app.js
@@ -13,6 +13,29 @@ app.use(cors());
 app.use(express.json());
 app.use(passport.initialize());
 
+passport.use(
+    new LocalStrategy(async (username, password, done) => {
+      try {
+        const user = await prisma.user.findUnique({
+          where: {
+            username: username,
+          },
+        });
+  
+        if (!user) {
+          return done(null, false, { message: "Incorrect username" });
+        }
+        const match = await bcrypt.compare(password, user.password);
+        if (!match) {
+          return done(null, false, { message: "Incorrect password" });
+        }
+        return done(null, user);
+      } catch (err) {
+        return done(err);
+      }
+    })
+);
+
 
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
