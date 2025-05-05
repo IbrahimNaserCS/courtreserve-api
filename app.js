4161e78753c3bdb4fe8d430f0317d899c0169622
Your Name <naseribrahim755@gmail.com>
2025-05-05 11:06:11 -0500

    add login and signup routes

--- app.js
+++ app.js
@@ -14,29 +14,53 @@ app.use(express.json());
 app.use(passport.initialize());
 
 passport.use(
-    new LocalStrategy(async (username, password, done) => {
-      try {
-        const user = await prisma.user.findUnique({
-          where: {
-            username: username,
-          },
-        });
-  
-        if (!user) {
-          return done(null, false, { message: "Incorrect username" });
-        }
-        const match = await bcrypt.compare(password, user.password);
-        if (!match) {
-          return done(null, false, { message: "Incorrect password" });
-        }
-        return done(null, user);
-      } catch (err) {
-        return done(err);
+  new LocalStrategy(async (username, password, done) => {
+    try {
+      const user = await prisma.user.findUnique({
+        where: {
+          username: username,
+        },
+      });
+
+      if (!user) {
+        return done(null, false, { message: "Incorrect username" });
       }
-    })
+      const match = await bcrypt.compare(password, user.password);
+      if (!match) {
+        return done(null, false, { message: "Incorrect password" });
+      }
+      return done(null, user);
+    } catch (err) {
+      return done(err);
+    }
+  })
 );
 
+app.post(
+  "/login",
+  passport.authenticate("local", { session: false }),
+  (req, res) => {
+    jwt.sign({ user: req.user }, process.env.SECRET_KEY, (err, token) => {
+      res.json({ token });
+    });
+  }
+);
+
+app.post("/signup", async (req, res, next) => {
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    const user = await prisma.user.create({
+      data: {
+        username: req.body.username,
+        password: hashedPassword,
+      },
+    });
+    res.status(200);
+  } catch (err) {
+    return next(err);
+  }
+});
 
 app.listen(port, () => {
-    console.log(`Server listening on port ${port}`);
-})
+  console.log(`Server listening on port ${port}`);
+});
