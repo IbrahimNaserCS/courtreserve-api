10ec4286324895fc47a6d3f4ba7d3a165076ee06
Your Name <naseribrahim755@gmail.com>
2025-05-05 10:57:58 -0500

    include all dependencies for login/signup

--- app.js
+++ app.js
@@ -2,7 +2,16 @@ const express = require("express");
 const app = express();
 require("dotenv").config();
 const port = process.env.PORT || 3000;
-
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const bcrypt = require("bcryptjs");
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+const jwt = require("jsonwebtoken");
+const cors = require("cors");
+app.use(cors());
+app.use(express.json());
+app.use(passport.initialize());
 
 
 app.listen(port, () => {
