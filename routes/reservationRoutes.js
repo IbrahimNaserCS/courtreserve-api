915e1150bdb91243a42e9fde44bbcd7a3e63d1f2
Your Name <naseribrahim755@gmail.com>
2025-05-06 15:26:26 -0500

    changed code to look in body for username not params

--- routes/reservationRoutes.js
+++ routes/reservationRoutes.js
@@ -4,16 +4,16 @@ const { verifyToken } = require("../auth/authfunction");
 const { getUserReservations, addReservation, deleteReservation } = require("../controllers/reservationController");
 const jwt = require("jsonwebtoken");
 
-reservationRouter.get("/:userid", verifyToken, async (req, res) => {
+reservationRouter.get("/:username", verifyToken, async (req, res) => {
     jwt.verify(req.token, process.env.SECRET_KEY, async (err, authData) => {
         if (err) {
             res.sendStatus(403);
         } else {
             const decoded = authData;
-            if (decoded.user.username !== req.params.userid) {
+            if (decoded.user.username !== req.params.username) {
                 res.sendStatus(403);
             } else {
-                const reservations = await getUserReservations(req.params.userid);
+                const reservations = await getUserReservations(req.params.username);
                 res.json(reservations);
             }
         }
@@ -26,7 +26,9 @@ reservationRouter.post("/", verifyToken, async (req, res) => {
             res.sendStatus(403);
         } else {
             const decoded = authData;
-            if (decoded.user.username !== req.params.userid) {
+            console.log("Decoded user username is", decoded.user.username);
+            console.log()
+            if (decoded.user.username !== req.body.username) {
                 res.sendStatus(403);
             } else {
                 await addReservation(req.body.courtId, req.body.username, req.body.date);
@@ -42,7 +44,7 @@ reservationRouter.delete("/:reservationid", verifyToken, async (req, res) => {
             res.sendStatus(403);
         } else {
             const decoded = authData;
-            if (decoded.user.username !== req.params.userid) {
+            if (decoded.user.username !== req.body.username) {
                 res.sendStatus(403);
             } else {
                 await deleteReservation(req.params.reservationid);
