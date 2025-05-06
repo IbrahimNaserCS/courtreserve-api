c65d441018c5a91b5e8337fec12c6070c0fcb533
Your Name <naseribrahim755@gmail.com>
2025-05-06 14:55:12 -0500

    added authentication to all routes

--- routes/courtRoutes.js
+++ routes/courtRoutes.js
@@ -2,15 +2,28 @@ const { Router } = require("express");
 const courtRouter = Router();
 const { getAllCourts, getReservedDates } = require("../controllers/courtController");
 const { verifyToken } = require("../auth/authfunction");
+const jwt = require("jsonwebtoken");
 
-courtRouter.get("/", async (req, res) => {
-    const courts = await getAllCourts();
-    res.json(courts);
+courtRouter.get("/", verifyToken, async (req, res) => {
+    jwt.verify(req.token, process.env.SECRET_KEY, async (err, authData) => {
+        if (err) {
+            res.sendStatus(403);
+        } else {
+            const courts = await getAllCourts();
+            res.json(courts);
+        }
+    });
 });
 
-courtRouter.get("/:courtid", async (req, res) => {
-    const reservedDate = await getReservedDates(req.params.courtid);
-    res.json(reservedDate);
-})
+courtRouter.get("/:courtid", verifyToken, async (req, res) => {
+    jwt.verify(req.token, process.env.SECRET_KEY, async (err, authData) => {
+        if (err) {
+            res.sendStatus(403);
+        } else {
+            const reservedDate = await getReservedDates(req.params.courtid);
+            res.json(reservedDate);
+        }
+    });
+});
 
 module.exports = courtRouter;
--- routes/reservationRoutes.js
+++ routes/reservationRoutes.js
@@ -2,20 +2,39 @@ const { Router } = require("express");
 const reservationRouter = Router();
 const { verifyToken } = require("../auth/authfunction");
 const { getUserReservations, addReservation, deleteReservation } = require("../controllers/reservationController");
+const jwt = require("jsonwebtoken");
 
-reservationRouter.get("/:userid", async (req, res) => {
-    const reservations = await getUserReservations(req.params.userid);
-    res.json(reservations);
+reservationRouter.get("/:userid", verifyToken, async (req, res) => {
+    jwt.verify(req.token, process.env.SECRET_KEY, async (err, authData) => {
+        if (err) {
+            res.sendStatus(403);
+        } else {
+            const reservations = await getUserReservations(req.params.userid);
+            res.json(reservations);
+        }
+    });
 });
 
-reservationRouter.post("/", async (req, res) => {
-    await addReservation(req.body.courtId, req.body.username, req.body.date);
-    res.sendStatus(200);
+reservationRouter.post("/", verifyToken, async (req, res) => {
+    jwt.verify(req.token, process.env.SECRET_KEY, async (err, authData) => {
+        if (err) {
+            res.sendStatus(403);
+        } else {
+            await addReservation(req.body.courtId, req.body.username, req.body.date);
+            res.sendStatus(200);
+        }
+    });
 });
 
-reservationRouter.delete("/:reservationid", async (req, res) => {
-    await deleteReservation(req.params.reservationid);
-    res.sendStatus(200);
+reservationRouter.delete("/:reservationid", verifyToken, async (req, res) => {
+    jwt.verify(req.token, process.env.SECRET_KEY, async (err, authData) => {
+        if (err) {
+            res.sendStatus(403);
+        } else {
+            await deleteReservation(req.params.reservationid);
+            res.sendStatus(200);
+        }
+    });
 })
 
 module.exports = reservationRouter;
