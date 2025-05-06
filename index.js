7139ae7a43aa35fb14f08b906878d2bf481de8f3
Your Name <naseribrahim755@gmail.com>
2025-05-06 11:17:27 -0500

    add routers into index.js

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const app = express();
 const passport = require("passport");
 const cors = require("cors");
 const userRoutes = require("./routes/userRoutes");
+const courtRoutes = require("./routes/courtRoutes");
+const reservationRoutes = require("./routes/reservationRoutes");
 require("dotenv").config();
 const port = process.env.PORT || 3000;
 
@@ -10,6 +12,8 @@ app.use(cors());
 app.use(express.json());
 app.use(passport.initialize());
 app.use("/users", userRoutes);
+app.use("/courts", courtRoutes);
+app.use("/reservations", reservationRoutes);
 
 module.exports = {
     app,
